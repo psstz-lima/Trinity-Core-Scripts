bd3ce402b328b86873562652e73a5d3999c41346
Neroxqt <tinydikfgt@gmail.com>
2015-07-29 18:05:35 -0400

    Updated Duel Reset.

---
+++ PVP/pvp_duel_reset.cpp
@@ -0,0 +1,34 @@
+/*
+<--------------------------------------------------------------------------->
+ - Developer(s): ??
+ - Updated By: Ghostcrawler336
+ - Complete: 100%
+ - ScriptName: 'Duel Reset'
+ - Comment: Tested by Nerox.
+ - Updated - 7/29/2015
+<--------------------------------------------------------------------------->
+*/
+#include "SpellHistory.h"
+ 
+class Duel_Reset : public PlayerScript
+{
+        public:
+                Duel_Reset() : PlayerScript("Duel_Reset"){}
+ 
+                void OnDuelEnd(Player* pWinner, Player* pLoser, DuelCompleteType /*type*/)
+                {
+						pWinner->GetSpellHistory()->ResetAllCooldowns();
+						pLoser->GetSpellHistory()->ResetAllCooldowns();
+                        pWinner->SetHealth(pWinner->GetMaxHealth());
+                        if ( pWinner->getPowerType() == POWER_MANA )
+                                pWinner->SetPower(POWER_MANA, pWinner->GetMaxPower(POWER_MANA));
+                        pLoser->SetHealth(pLoser->GetMaxHealth());
+                        if ( pLoser->getPowerType() == POWER_MANA )
+                                pLoser->SetPower(POWER_MANA,  pLoser->GetMaxPower(POWER_MANA));
+                }
+};
+ 
+void AddSC_Duel_Reset()
+{
+        new Duel_Reset();
+}
